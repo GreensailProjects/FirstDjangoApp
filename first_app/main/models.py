02898ec17593b33b6c933bcd4c076b2604088f17
greensail <greensails72@gmail.com>
2023-10-12 13:32:58 +0300

    Initial commit

---
+++ first_app/main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Topic (models.Model):
+    text = models.CharField (max_length=200)
+    date_added=models.DateField(auto_now=True)
+    def __str__(self):
+        return self.text
