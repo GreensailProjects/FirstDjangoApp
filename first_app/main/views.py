76d6b8a780d1d44c674b33966556e5e5a61def51
greensail <greensails72@gmail.com>
2023-10-18 16:31:05 +0300

    Added form for topic

--- first_app/main/views.py
+++ first_app/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -13,11 +14,24 @@ def topics(request):
     context = {'topics':topics}
     return render(request, 'main/topics.html', context)
 
+
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {
-        'topic':topic,
-        'entries':entries
+        'topic': topic,
+        'entries': entries
     }
     return render(request, 'main/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:topics')
+    context = {'form': form}
+    return render(request, 'main/new_topic.html', context)
